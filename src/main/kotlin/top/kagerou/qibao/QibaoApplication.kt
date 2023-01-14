420f7370d30de86258572a8984f1ca873415ef8f
xiaohuliqibao <xiaohuliqibao@gmail.com>
2023-01-15 00:39:01 +0800

    init a spring demo

---
+++ src/main/kotlin/top/kagerou/qibao/QibaoApplication.kt
@@ -0,0 +1,11 @@
+package top.kagerou.qibao
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class QibaoApplication
+
+fun main(args: Array<String>) {
+	runApplication<QibaoApplication>(*args)
+}
---
+++ src/test/kotlin/top/kagerou/qibao/QibaoApplicationTests.kt
@@ -0,0 +1,13 @@
+package top.kagerou.qibao
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class QibaoApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
